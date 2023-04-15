47b794d5bd29644cdb7719d4ab540f69e95c5475
Reed Martin <reedhmartin@gmail.com>
2023-04-15 09:15:23 -0400

    config.js - Defined associations between models.

--- config/config.js
+++ config/config.js
@@ -22,4 +22,29 @@ db.User = require('../models/User')(sequelize, Sequelize);
 db.Post = require('../models/Post')(sequelize, Sequelize);
 db.Comment = require('../models/Comment')(sequelize, Sequelize);
 
+// Define associations between models
+db.User.hasMany(db.Post, {
+  foreignKey: 'user_id'
+});
+
+db.Post.belongsTo(db.User, {
+  foreignKey: 'user_id'
+});
+
+db.Post.hasMany(db.Comment, {
+  foreignKey: 'post_id'
+});
+
+db.Comment.belongsTo(db.Post, {
+  foreignKey: 'post_id'
+});
+
+db.User.hasMany(db.Comment, {
+  foreignKey: 'user_id'
+});
+
+db.Comment.belongsTo(db.User, {
+  foreignKey: 'user_id'
+});
+
 module.exports = sequelize;
